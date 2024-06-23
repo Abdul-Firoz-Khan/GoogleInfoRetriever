3a4dd9ae637ce00a02e477434b3e718a9515f6a8
Abdul-Firoz-Khan <165898558+Abdul-Firoz-Khan@users.noreply.github.com>
2024-06-23 18:06:21 +0530

    first commit

---
+++ src/main/java/GoogleInfoRetriever.java
@@ -0,0 +1,37 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GoogleInfoRetriever {
+
+    public static void main(String[] args) {
+        // Setup WebDriverManager
+        WebDriverManager.firefoxdriver().setup();
+
+        // Create a new instance of ChromeDriver
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+            // Navigate to Google
+            driver.get("https://www.google.com");
+
+            // Get the title of the page
+            String title = driver.getTitle();
+            System.out.println("Title: " + title);
+
+            // Get the current URL
+            String url = driver.getCurrentUrl();
+            System.out.println("URL: " + url);
+
+            // Get the page source
+//            String pageSource = driver.getPageSource();
+//            System.out.println("Page Source: ");
+//            System.out.println(pageSource);
+
+        } finally {
+            // Close the browser
+            driver.quit();
+        }
+    }
+}
